3dc071924d3f75a2c66177ec2421212109edea27
valentinesamuel <valentinesamuel2580@gmail.com>
2022-09-15 18:52:04 +0100

    added cookie sessions

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const passport = require('passport')
 const { Strategy } = require('passport-google-oauth20')
 const express = require('express')
 const helmet = require('helmet')
+const cookieSession = require('cookie-session')
 require('dotenv').config()
 const PORT = 3000
 
 
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: COOKIE_KEY_2
 }
 
 const AUTH_OPTIONS = {
@@ -30,6 +33,11 @@ passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
 const app = express()
 app.use(helmet())
 
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2]
+}))
 app.use(passport.initialize())
 
 function checkLoggedIn(req, res, next) {
